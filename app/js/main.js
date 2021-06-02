60625dee5f41c89377b2af76d6a9cc05650096e4
thuysddoox <dothuy302000@gmail.com>
2021-06-02 10:21:28 +0700

    fix some bug about display register and  index.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
 var register = document.querySelector('.register');
 var login = document.querySelector('.login');
 var choice = document.querySelector('.choice');
+var bar = document.querySelector('.bar__content');
+
 
 function showRegister() {
     if (register.style.display !== "block") {
@@ -24,4 +26,10 @@ function showChoice() {
         login.style.display = "none";
         register.style.display = "none";
     } else choice.style.display = "none";
+}
+
+function showBarBtn() {
+    if (bar__content.style.left !== "0px")
+        bar__content.style.left = "0px";
+    else bar__content.style.left = "-300px";
 }
